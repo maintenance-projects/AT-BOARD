6b4514a6df758983d754d0136c336dab5405599f
hyunjunny <hjkim@ultari.co.kr>
2026-01-15 17:00:36 +0900

    init

---
+++ src/main/java/kr/co/ultari/at_board/AtBoardApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ultari.at_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/ultari/at_board/AtBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ultari.at_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
